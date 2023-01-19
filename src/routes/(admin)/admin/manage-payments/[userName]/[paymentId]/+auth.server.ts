4f9e8a6fc1e12c061405662fde1381f57f3209aa
Ivan Hofer <ivan.hofer@outlook.com>
2023-01-19 17:06:29 +0100

    add optional comment

--- src/routes/(admin)/admin/manage-payments/[userName]/[paymentId]/+auth.server.ts
+++ src/routes/(admin)/admin/manage-payments/[userName]/[paymentId]/+auth.server.ts
@@ -10,4 +10,8 @@ export const guard = async (event: RequestEvent) => {
 
 	if (!await doesPaymentWithIdForUserExist(event.params.userName, paymentId))
 		throw error(404)
+
+	// optional:
+	// the auth guard could return some data, that gets passed to all `load` functions on the current level and below the folder tree
+	// because some auth checks could already fetch some data from the DB, passing it to the `load` function would speed up requests, because they don't need to be made twice
 }
